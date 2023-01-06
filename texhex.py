2c988f5f5886fc85fe45e288c5367d9d3b058b49
ez3mf <118151594+n3trunr@users.noreply.github.com>
2023-01-05 23:47:33 -0500

    added cli components

--- texhex.py
+++ texhex.py
@@ -12,6 +12,27 @@ def txhx(tx:str):
     for c in tx:
         dec = ord(c)
         hx += hex(dec)[2:]
-    print(hx)  
+    return hx  
+
+# importing required modules
+import argparse
+
+# create a parser object
+parser = argparse.ArgumentParser(description = "A text to hex converter")
+
+# add argument
+parser.add_argument("txhx", nargs = '*', metavar = "txhx", type = str,
+					help = "gimme text i give u hex")
+
+# parse the arguments from standard input
+args = parser.parse_args()
+
+# check if add argument has any input data.
+# If it has, then print sum of the given numbers
+output = ''
+if len(args.txhx) != 0:
+    for word in args.txhx:
+	    output += txhx(word) + ' '
+print(output)
 
   
